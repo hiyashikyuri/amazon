e646a89519375201fe947375811d6f9d1bbd8498
Masahiro Okubo <stmhamachiii@gmail.com>
2021-01-31 23:31:31 +0900

    Headerの追加

---
+++ src/firebase.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBX05gHF0tMBW44HTZqsXhFFmj361RnEWQ",
+    authDomain: "fir-8876a.firebaseapp.com",
+    projectId: "fir-8876a",
+    storageBucket: "fir-8876a.appspot.com",
+    messagingSenderId: "520352910446",
+    appId: "1:520352910446:web:77c92cc258060249302a7f",
+    measurementId: "G-LYF7ZCKDY1"
+};
