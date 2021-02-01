42a5d4b1f00fdd499469eadaf162b3bd5d4cebf6
Masahiro Okubo <stmhamachiii@gmail.com>
2021-02-01 23:26:44 +0900

    Homeの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 require('dotenv').config();
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
